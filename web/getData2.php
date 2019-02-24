63dc82abd50e09c95f7314deb03b3f96d9de2b11
RS <eromser@gmail.com>
2019-02-25 01:27:52 +0300

    fixes in JavaScript data part

--- web/getData2.php
+++ web/getData2.php
@@ -5,7 +5,7 @@ include 'db.php';
 // This is just an example of reading server side data and sending it to the client.
 // It reads a json formatted text file and outputs it.
 
-  $sqlq = "SELECT DATE_FORMAT(`logdate`, '%H:%i') AS time, temperature, humidity
+  $sqlq = "SELECT DATE_FORMAT(`logdate`, '%H:%i') AS time, temperature, humidity, heap
 		FROM  `DataTable` 
 		WHERE DATE( logdate ) = CURDATE( )   order by id desc limit 100";
 
@@ -21,16 +21,12 @@ include 'db.php';
 
 
 $string = '{
-  "cols": [
-        {"id":"","label":"Log Time","pattern":"","type":"string"},
-        {"id":"","label":"Temperature","pattern":"","type":"number"},
-        {"id":"","label":"Humidity","pattern":"","type":"number"}
-      ],
-  "rows": [';
-
-  if ( $result->num_rows  > 0 ) {
+  "cols": [        {"id":"","label":"Log Time","pattern":"","type":"string"},        {"id":"","label":"Temperature","pattern":"","type":"number"},        {"id":"","label":"Humidity","pattern":"","type":"number"},        {"id":"","label":"Heap","pattern":"","type":"number"}      ],  "rows": [';
+  $i = $result->num_rows;
+  if ( $i > 0 ) {
     while($row = mysqli_fetch_array($result)) {
-    	$string .= '{"c":[{"v":"'. $row['time'] .'","f":null},{"v":'. $row['temperature'] .',"f":null},{"v":'. $row['humidity'] .',"f":null}]},';
+    	$string .= '{"c":[{"v":"'. $row['time'] .'"},{"v":'. $row['temperature'] .'},{"v":'. $row['humidity'] .'},{"v":'. $row['heap'] .'}]}';
+	if (--$i > 0) $string .= ',';
     }
   }
   
