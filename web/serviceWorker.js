c91c2815f77d5e81aa545a0fe456354f9a2f0b65
RS <eromser@gmail.com>
2019-10-08 01:10:16 +0300

    PWA and Google Lighthouse recommendations applied

---
+++ web/serviceWorker.js
@@ -0,0 +1,39 @@
+const staticAssets=[
+    './Inconsolata-Regular.ttf',
+    './view2.html',
+    './Icon-192px.png',
+    './Icon-512px.png'
+];
+
+self.addEventListener('install', async event=>{
+    const cache = await caches.open('static-cache');
+    cache.addAll(staticAssets);
+});
+
+self.addEventListener('fetch', event => {
+    const req = event.request;
+    const url = new URL(req.url);
+
+    if(url.origin === location.url){
+        event.respondWith(cacheFirst(req));
+    } else {
+        event.respondWith(newtorkFirst(req));
+    }
+});
+
+async function cacheFirst(req){
+    const cachedResponse = caches.match(req);
+    return cachedResponse || fetch(req);
+}
+
+async function newtorkFirst(req){
+    const cache = await caches.open('dynamic-cache');
+
+    try {
+        const res = await fetch(req);
+        cache.put(req, res.clone());
+        return res;
+    } catch (error) {
+        return await cache.match(req);
+    }
+}
